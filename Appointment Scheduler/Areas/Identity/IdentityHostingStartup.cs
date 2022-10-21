35a3e2e64f937308746126f6e0e309d101e507b8
Mohammad sajjadian <2222h.com@gmail.com>
2022-10-21 12:46:15 +0330

    Create Database & Layering our Entities.

---
+++ Appointment Scheduler/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Appointment_Scheduler.Areas.Identity.Data;
+using Appointment_Scheduler.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Appointment_Scheduler.Areas.Identity.IdentityHostingStartup))]
+namespace Appointment_Scheduler.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DBscheduler>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DBschedulerConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DBscheduler>();
+            });
+        }
+    }
+}
--- Appointment Scheduler/Startup.cs
+++ Appointment Scheduler/Startup.cs
@@ -1,6 +1,8 @@
+using Appointment_Scheduler.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,13 +22,17 @@ namespace Appointment_Scheduler
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Connection String Setting
+            services.AddDbContext<DBscheduler>(x =>
+            {
+                x.UseSqlServer(Configuration.GetConnectionString("DBscheduler"));
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +42,6 @@ namespace Appointment_Scheduler
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
