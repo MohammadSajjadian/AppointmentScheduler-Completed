7c683ac9e2eb92f016895661bf469af3b26548a8
Mohammad sajjadian <2222h.com@gmail.com>
2022-10-21 12:03:08 +0330

    Add project files.

---
+++ Appointment Scheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Appointment_Scheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
