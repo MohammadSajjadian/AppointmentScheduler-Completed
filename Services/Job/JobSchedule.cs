68d64b69ded41b0a49f9686af57e88542c242cf2
Mohammad sajjadian <2222h.com@gmail.com>
2022-10-28 17:04:23 +0330

    Create job folder for schedule appointment and Quartz configuration.

---
+++ Services/Job/JobSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Job
+{
+    public class JobSchedule
+    {
+        public Type jobType { get; set; }
+
+        public string cronExpression { get; set; }
+
+        public JobSchedule(Type _jobType, string _cronExpression)
+        {
+            jobType = _jobType;
+            cronExpression = _cronExpression;
+        }
+    }
+}
---
+++ Services/Job/SmsJob.cs
@@ -0,0 +1,58 @@
+﻿using Appointment_Scheduler.Data;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+using SmsIrRestful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Job
+{
+    [DisallowConcurrentExecution]
+    public class SmsJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            var option = new DbContextOptionsBuilder<DBscheduler>();
+            option.UseSqlServer("Server=MOHAMMAD-PC2\\MSSQLSERVER2019;Database=DBscheduler;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            using (DBscheduler db = new(option.Options))
+            {
+                db.appointments.Include(x => x.applicationUser).ToList();
+                List<Appointment> appointments = db.appointments.Where(x => x.time.Date.Date == DateTime.Now.Date.Date && x.time.Hour == DateTime.Now.Hour && x.time.Minute == DateTime.Now.Minute).ToList();
+
+                foreach (var item in appointments)
+                {
+                    #region Send SMS
+
+                    var token = new Token().GetToken("42f2ea6a34b93ff4dca9be13", "cxf9uY#%+zfU@^VF");
+
+                    var messageSendObject = new MessageSendObject()
+                    {
+                        Messages = new List<string> { $"قرار شما فرا رسید!\n'{item.description}'" }.ToArray(),
+                        MobileNumbers = new List<string> { item.applicationUser.PhoneNumber }.ToArray(),
+                        LineNumber = "30005680000578",
+                        SendDateTime = DateTime.Now,
+                        CanContinueInCaseOfError = false,
+                    };
+
+                    MessageSendResponseObject messageSendResponseObject = new MessageSend().Send(token, messageSendObject);
+
+                    #endregion
+
+                    #region Delete Appointment
+
+                    db.Remove(item);
+
+                    #endregion
+                }
+
+                db.SaveChanges();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
